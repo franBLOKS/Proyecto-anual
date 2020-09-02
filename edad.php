84d2ed30c6caa21b603c4ea6402a9bd23010963b
franBLOKS <57075981+franBLOKS@users.noreply.github.com>
2020-09-02 20:45:21 -0300

    comienzo
    esto es basicamente el inicio de la pagina web

---
+++ edad.php
@@ -0,0 +1,9 @@
+<?php
+
+$e = $_POST["edad"];
+if($e<18){
+	echo "tu edad es ".e." por lo tanto eres menor, asi que procura aprender con supervision de un mayor"
+}else{
+	echo "tu edad es ".e." por lo tanto eres mayor"
+}
+?>
