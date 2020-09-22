64a657709ad242316538d107de6337f1199cbab2
franBLOKS <57075981+franBLOKS@users.noreply.github.com>
2020-09-22 14:50:03 -0300

    login
    login

---
+++ login php/index.php
@@ -0,0 +1,15 @@
+
+<html>
+    <head>
+        <title>Bienvenido</title>
+        <meta charset="utf-8">
+        <link href="https://fonts.googleapis.com/css?family-Roboto" rel-"stylesheet">
+        <link rel="stylesheet" href="assets/css">
+    </head>
+<body>
+<h1>Por favor registrate o inicia secion</h1>
+
+<a href="login.php">Iniciar secion</a> or
+<a href="signup.php">Registrarse</a>
+</body>
+</html>
