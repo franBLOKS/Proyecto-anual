134af1ad3d339a4c3ce760c9776d72eeedbc83e0
Santiago Codina <67075891+OverUrHead@users.noreply.github.com>
2020-11-08 03:48:23 -0300

    perdon, me mande una macana

---
+++ login php(NUEVO DISEÑO)/signup.php
@@ -0,0 +1,40 @@
+<?php
+  require 'database.php';
+  $message = '';
+  if (!empty($_POST['email']) && !empty($_POST['password'])) {
+    $sql = "INSERT INTO users (email, password) VALUES (:email, :password)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam(':email', $_POST['email']);
+    $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
+    $stmt->bindParam(':password', $password);
+    if ($stmt->execute()) {
+      $message = 'Nuevo usuario creado';
+    } else {
+      $message = 'Hemos tenido un error';
+    }
+  }
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Registrarse</title>
+    <link href="https://fonts.googleapis.com/css2?family=Patua+One&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;1,100;1,300;1,400&display=swap" rel="stylesheet">    
+    <link rel="stylesheet" href="assets\style_signup.css">
+  </head>
+  <body>
+    <?php if(!empty($message)): ?>
+      <p> <?= $message ?></p>
+    <?php endif; ?>
+    <h1>Registrarse</h1>
+    
+    <form action="signup.php" method="POST">
+      <input name="email" type="text" placeholder="Ingrese su correo">
+      <input name="password" type="password" placeholder="Ingrese su contraseña">
+      <input name="confirm_password" type="password" placeholder="Confirme su contraseña">
+      <center><input type="submit" value="Inscribirse"></center>
+      <center><span class="boton"><a href="login.php" >¿Ya tenes cuenta? ¡Inicia sesión!</a></span></center>
+    </form>
+  </body>
+</html>
