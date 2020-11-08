cbcd1a06e2834aa247c406aa81f97e455cb6f05d
Nahuel Laudani <nahuel16xd@gmail.com>
2020-11-08 04:02:36 -0300

    Lista Beta 2.0
    Muestra si no hay coincidencias

--- Lista (BETA)/lista.php
+++ Lista (BETA)/lista.php
@@ -1,22 +1,19 @@
 <!DOCTYPE html>
 <html>
     <head>
-        
+
         <link href="https://fonts.googleapis.com/css2?family=Patua+One&display=swap" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;1,100;1,300;1,400&display=swap" rel="stylesheet">
         <link rel="stylesheet" type="text/css" href="esteticaL.css" media="screen">
         <meta charset="utf-8">
-
+        
     </head>
-    <body>    
+    <body>
         <h1 class="titulo">Resultados...</h1>
 
-        <div>
-        <input type="submit" value="busqueda" href="C:/Users/franBLOKS/Desktop/6to%20C/So/Proyecto-anual/Menú%20(BETA)/index.html">
-        </div>
-        
          <?php
-        
+        error_reporting (E_ALL ^ E_NOTICE);
+
         $servername = "localhost";
         $username = "root";
         $password = "";
@@ -33,22 +30,32 @@
     
         $receta = $_POST['busqueda'];
 
-
-        $consulta = "SELECT * FROM receta WHERE NOMBRE_RECETA LIKE '%$receta%'";
-
+        $consulta = "SELECT * FROM receta WHERE INGREDIENTES LIKE '%$receta%'";
 
         $datos = mysqli_query($conexion,$consulta);
 
         //$datos=($_GET["datos"]);
 
+            $noresult=0;
+
             while ($fila=mysqli_fetch_array($datos)){;?>
                 
                 <div>
                      <label for="label"><h2><?php $nombre = $fila["NOMBRE_RECETA"]; echo "$nombre";?></h2><p><?php $descripcion = $fila["DESCRIPCION"]; echo "$descripcion";?></p></label>
-                    <input type="button" id="label" class="boton" action="">
+                    <input type="button" id="label" class="boton">
                  </div>
                  
-                 <?php };?>
+                 <?php if($fila!=""){
+
+                    $noresult=1;
+                 }
+                }
+
+                if($noresult==0){
+                    ?> <h5 class="titulo">No encontramos resultados.</h5>" <?php
+                }
+                 
+                 ?>
                
         
     </body>
