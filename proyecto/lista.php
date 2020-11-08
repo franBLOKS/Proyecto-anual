33364ec0d99b1670323b571ac21c7c76ce293bbd
Nahuel Laudani <nahuel16xd@gmail.com>
2020-11-08 05:11:11 -0300

    Lista 2.0
    Nueva función para detectar si hay o no resultados

--- proyecto/lista.php
+++ proyecto/lista.php
@@ -4,7 +4,7 @@
 
         <link href="https://fonts.googleapis.com/css2?family=Patua+One&display=swap" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;1,100;1,300;1,400&display=swap" rel="stylesheet">
-        <link rel="stylesheet" type="text/css" href="style_lista.css" media="screen">
+        <link rel="stylesheet" type="text/css" href="esteticaL.css" media="screen">
         <meta charset="utf-8">
         
     </head>
@@ -13,6 +13,7 @@
 
          <?php
         error_reporting (E_ALL ^ E_NOTICE);
+
         $servername = "localhost";
         $username = "root";
         $password = "";
@@ -29,22 +30,32 @@
     
         $receta = $_POST['busqueda'];
 
-
-        $consulta = "SELECT * FROM receta WHERE NOMBRE_RECETA LIKE '%$receta%'";
-
+        $consulta = "SELECT * FROM receta WHERE INGREDIENTES LIKE '%$receta%'";
 
         $datos = mysqli_query($conexion,$consulta);
 
         //$datos=($_GET["datos"]);
 
+            $noresult=0;
+
             while ($fila=mysqli_fetch_array($datos)){;?>
                 
                 <div>
                      <label for="label"><h2><?php $nombre = $fila["NOMBRE_RECETA"]; echo "$nombre";?></h2><p><?php $descripcion = $fila["DESCRIPCION"]; echo "$descripcion";?></p></label>
-                    <input type="button" id="label" class="boton" action="">
+                    <input type="button" id="label" class="boton">
                  </div>
                  
-                 <?php };?>
+                 <?php if($fila!=""){
+
+                    $noresult=1;
+                 }
+                }
+
+                if($noresult==0){
+                    ?> <h5 class="titulo">No encontramos resultados.</h5>" <?php
+                }
+                 
+                 ?>
                
         
     </body>
